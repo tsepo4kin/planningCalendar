0766ac7c57b1f39bc85f0b4a8cb38bbe3c3cc83e
Kirill <kupuk132@gmail.com>
2020-09-15 19:16:25 +0300

    state dayPlan change

--- src/store/dayPlan.js
+++ src/store/dayPlan.js
@@ -1,18 +1,34 @@
 export default {
   state: {
     tasks: [],
+    isLocal = false
   },
   mutations: {
     setTasks(state, payload) {
       state.tasks = payload;
     },
+    localToggle(state) {
+      state.isLocal = !state.isLocal
+    },
+    deleteTasks(state, payload) {
+      state.tasks = state.tasks.filter(e => e.id !== payload)
+    }
   },
   actions: {
-    saveTasks() {},
+    saveTasks({commit}) {
+      if(commit('getSaveSettings')) {
+        //save in lockalstorage
+      } else {
+        //save in database
+      }
+    },
     deleteTasks() {},
-    sortTasks() {},
+    localToggle({commit}) {
+      commit('localToggle')
+    }
   },
   getters: {
     getTasks: (state) => state.tasks,
+    getSaveSettings: (state) => state.isLocal
   },
 };
