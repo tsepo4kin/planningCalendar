513b65d3697edf0b43fd562df560c65370b53b6e
Kirill <kupuk132@gmail.com>
2020-09-17 02:30:19 +0300

    dayPlan store test version

--- src/store/dayPlan.js
+++ src/store/dayPlan.js
@@ -12,15 +12,28 @@ export default {
     },
     deleteTasks(state, payload) {
       state.tasks = state.tasks.filter(e => e.id !== payload)
+    },
+    addTask(state, payload) {
+      state.tasks.push(payload)
     }
   },
   actions: {
-    saveTasks({commit}) {
+    saveTasks({commit},state) {
       if(commit('getSaveSettings')) {
-        //save in lockalstorage
+        localStorage.setItem(tasks, state.tasks)
       } else {
         //save in database
       }
+    },
+    setTasks({commit}){
+      if(commit('getSaveSettings')) {
+        commit('setTasks', localStorage.getItem('tasks'))
+      } else {
+        //set from database
+      }
+    },
+    addTask({commit}, payload) {
+      commit('addTask', payload)
     },
     deleteTasks() {},
     localToggle({commit}) {
