4e71838d4c084e222b5c626789a24c84a31a750b
Kirill <kupuk132@gmail.com>
2020-09-22 02:49:14 +0300

    create cashRecord store module

---
+++ src/store/cashRecord.js
@@ -0,0 +1,79 @@
+export default {
+  state: {
+    incomeCategories: [
+
+    ],
+    outcomeCategories: [
+
+    ],
+  },
+  mutations: {
+    deleteIncomeById(state, id) {
+      state.incomeCategories = state.incomeCategories.filter(e => e.id !== id)
+    },
+    deleteOutcomeById(state, id) {
+      state.outcomeCategories = state.outcomeCategories.filter(e => e.id !== id)
+    },
+    setIncome(state, payload) {
+      state.incomeCategories = payload
+    },
+    setOutcome(state, payload) {
+      state.outcomeCategories = payload
+    },
+    addIncome(state, payload) {
+      state.incomeCategories.push(payload)
+    },
+    addOutcome(state, payload) {
+      state.outcomeCategories.push(payload)
+    }
+  },
+  actions: {
+    localToggle({commit}) {
+      commit('localToggle')
+    },
+    setIncome({commit, state}) {
+      if(state.isLocal) {
+        commit('setIncome', localStorage)
+      } else {
+        // from db
+      }
+    },
+    setOutcome({commit, state}) {
+      if(state.isLocal) {
+        commit('setOutcome', localStorage)
+      } else {
+        // from db
+      }
+    },
+    saveIncome({state}) {
+      if(state.isLocal) {
+        //save in localstorage
+      } else {
+        //save in db
+      }
+    },
+    saveOutcome({state}) {
+      if(state.isLocal) {
+        //save in localstorage
+      } else {
+        //save in db
+      }
+    },
+    addIncome({commit}, payload) {
+      commit('addIncome', payload)
+    },
+    addOutcome({commit}, payload) {
+      commit('addOutcome', payload)
+    },
+    deleteIncome({commit}, id) {
+      commit('deleteIncomeById', id)
+    },
+    deleteOutcome({commit}, id) {
+      commit('deleteOutcomeById', id)
+    }
+  },
+  getters: {
+    getIncome: (state) => state.incomeCategories,
+    getOutcome: (state) => state.outcomeCategories,
+  }
+}
