e199c6c1f8c17941d8f20ad6bfbece390edf1dc4
Kirill <kupuk132@gmail.com>
2020-10-08 22:32:19 +0300

    add habits store module

---
+++ src/store/habits.js
@@ -0,0 +1,44 @@
+export default {
+  state: {
+    habits:[
+
+    ],
+    isLocal: true,
+  },
+  mutations: {
+    setHabits(state, payload) {
+      state.habits = habits;
+    },
+    deleteHabit(state, payload) {
+      state.habits = state.habits.filter((e) => e.id !== payload);
+    },
+    addHabits(state, payload) {
+      state.habits.push(payload);
+    },
+  },
+  actions: {
+    saveHabits({ state }) {
+      if (state.isLocal) {
+        localStorage.setItem("habits", JSON.stringify(state.tasks));
+      } else {
+        //save in database
+      }
+    },
+    setHabits({ commit, state }) {
+      if (state.isLocal) {
+        commit("setTasks", JSON.parse(localStorage.getItem("habits")));
+      } else {
+        //set from database
+      }
+    },
+    addHabit({ commit }, payload) {
+      commit("addHabit", payload);
+    },
+    deleteHabitById({ commit }, payload) {
+      commit("deleteHabits", payload);
+    },
+  },
+  getters: {
+    getHabits: (state) => state.habits,
+  },
+};
